91753ae06fb9155974efe5770c65476ec6ce43a3
Desu Sai Kumar <desusaikumar@yahoo.com>
2018-01-10 21:45:02 +0530

    Adding more example of typescript

--- lesson7.ts
+++ lesson7.ts
@@ -3,3 +3,16 @@ interface player{
      addLives(n:number):void;
      score():number;
 }
+interface Employee{
+     name:String;
+     run():void;
+     addLives(n:number):void;
+     score():number;
+}
+interface Batsmen{
+     name:String;
+     run():void;
+     addLives(n:number):void;
+     score():number;
+     static Dept:String;
+}
