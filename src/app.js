06726ec54248fd34b2c938a786d2acfd7b7ace7a
Desu Sai Kumar <desusaikumar@yahoo.com>
2020-04-21 17:39:19 +0530

    Updating the modules as angular services

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
-document.addEventListener('DOMContentLoaded', function(){
-  document.body.innerHTML = d3.version;
-})
+var module = angular.module('d3-kit',[]);
+module.service('$d3', function(){
+  var tempD3Object = angular.copy(d3);
+  delete window['d3'];
+  return tempD3Object;
+});
+module.service('$lodash', function(){
+  var tempLodashObject = angular.copy(_);
+  delete window['_'];
+  return tempLodashObject;
+});
+module.run(['$d3', '$lodash', function($d3, $lodash){
+  console.log('Lodash version --> ' + $lodash.VERSION);
+  console.log('D3 version --> ' + $d3.version);
+}]);
