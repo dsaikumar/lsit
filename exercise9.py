e95ee22612e429a9c422b43adf1cf786dbccb056
dsaikumar <desusaikumar@yahoo.com>
2025-09-08 22:43:00 -0400

    Adding the code of exercise 9

--- exercise9.py
+++ exercise9.py
@@ -8,12 +8,13 @@ print(greet())  # None will return here
 greet()  # Peps will add above two lines 5 and 6 as empty for function
 
 
-def greeting(first_name, last_name):
+def greeting(first_name, last_name=''):  # last_name is optional
     print(f"Hi {first_name} {last_name}")
     print("Welcome aboard")
 
 
 greeting('Sai Kumar', 'Desu')
+greeting('Sai Kumar')
 
 # Polymorphism of overloading is not allowed
 
@@ -23,3 +24,15 @@ def get_greeting(name):
 
 
 print(get_greeting('Sai'))
+
+
+def multiple(*numbers):  # more than one argument is added to numbers
+    print(numbers)  # It is a tuple
+    total = 1
+    for x in numbers:
+        total = x*total
+    print(total)
+    return total
+
+
+print(multiple(2, 3, 4, 5))
